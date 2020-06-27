afa7447abc56a7354aa0c0d682722ca2d7ef5b6a
Taneli Salonen <taneli.salonen@fluidogroup.onmicrosoft.com>
2020-06-27 14:37:32 +0300

    fetch tweet text with chromeless

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const { Chromeless } = require('chromeless')
 
 const url = 'https://twitter.com/MariaOhisalo/status/1276833682775576582'
- 
+const urls = ['https://twitter.com/MariaOhisalo/status/1276833682775576582']
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+
 async function run() {
   const chromeless = new Chromeless()
- 
-  const tweetHtml = await chromeless
-    .goto(url)
-    .wait('article[role="article"]')
-    .html()
- 
-  console.log(tweetHtml) // prints local file path or S3 url
- 
+
+  // fetch the title of each tweet page
+  const tweetPromises = []
+  urls.forEach(url => {
+    const tweetTitle = chromeless
+      .setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
+      .goto(url)
+      .wait('article[role="article"]')
+      .evaluate(() => {
+        return document.title.split('"')[1] // the actual tweet content
+      })
+
+    tweetPromises.push(tweetTitle)
+  })
+
+  Promise.all(tweetPromises)
+    .then((title) => {
+      console.log(title);
+    })
+    .catch(error => { 
+      console.error(error.message)
+    });
+
   await chromeless.end()
 }
- 
+
 run().catch(console.error.bind(console))
