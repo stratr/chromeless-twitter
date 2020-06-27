9bde490f67640ff81039b711697b0cda22021a70
Taneli Salonen <taneli.salonen@fluidogroup.onmicrosoft.com>
2020-06-27 15:25:16 +0300

    try to go through multiple pages at once

--- index.js
+++ index.js
@@ -1,35 +1,102 @@
 const { Chromeless } = require('chromeless')
+const { BigQuery } = require('@google-cloud/bigquery');
+const { evaluate } = require('chromeless/dist/src/util');
+const bigquery = new BigQuery();
 
-const url = 'https://twitter.com/MariaOhisalo/status/1276833682775576582'
-const urls = ['https://twitter.com/MariaOhisalo/status/1276833682775576582']
+const datasetId = 'tweets_eu'
+const tableId = 'chromeless_tweets'
+
+const urls = ['https://twitter.com/MariaOhisalo/status/1276833682775576582', 'https://twitter.com/hjallisharkimo/status/1276567729739386887']
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 
-async function run() {
-  const chromeless = new Chromeless()
-
-  // fetch the title of each tweet page
-  const tweetPromises = []
-  urls.forEach(url => {
-    const tweetTitle = chromeless
-      .setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
-      .goto(url)
-      .wait('article[role="article"]')
-      .evaluate(() => {
-        return document.title.split('"')[1] // the actual tweet content
-      })
-
-    tweetPromises.push(tweetTitle)
+function sleep() {
+  return new Promise(resolve => {
+    setTimeout(resolve, 2000)
   })
-
-  Promise.all(tweetPromises)
-    .then((title) => {
-      console.log(title);
-    })
-    .catch(error => { 
-      console.error(error.message)
-    });
-
-  await chromeless.end()
 }
 
-run().catch(console.error.bind(console))
+async function run() {
+  try {
+    const masterChromeless = new Chromeless()
+
+    await sleep() // Needed to wait for Chrome to start up
+
+    const promises = urls.map(url => {
+      return new Promise((resolve, reject) => {
+        const chromeless = new Chromeless({ launchChrome: false })
+        chromeless
+          .setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
+          .goto(url)
+          .wait('article[role="article"]')
+          .evaluate(() => {
+            return document.title.split('"')[1] // the actual tweet content
+          })
+          .then(async evaluate => {
+            await chromeless.end()
+            resolve(evaluate)
+          })
+          .catch(err => reject(err))
+      })
+    })
+
+    const tweets = await Promise.all(promises)
+
+    tweets.forEach(tweet => console.log(`Tweet: ${tweet}`))
+
+    //await masterChromeless.end()
+  } catch (err) {
+    console.error('error: ' + err)
+  }
+}
+
+run()
+
+// async function run() {
+//   // fetch the title of each tweet page
+//   const tweetPromises = []
+//   urls.forEach(url => {
+//     const chromeless = new Chromeless()
+//     const tweetTitle = chromeless
+//       .setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
+//       .goto(url)
+//       .wait('article[role="article"]')
+//       .evaluate(() => {
+//         return document.title.split('"')[1] // the actual tweet content
+//       })
+
+//     tweetPromises.push(tweetTitle)
+//   })
+
+//   Promise.all(tweetPromises)
+//     .then((tweets) => {
+//       console.log(tweets);
+//     })
+//     .catch(error => {
+//       console.error(error.message)
+//     });
+
+//   await chromeless.end()
+// }
+
+async function insertRowsAsStream(rows) {
+  // Inserts the JSON objects into my_dataset:my_table.
+
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  // const datasetId = 'my_dataset';
+  // const tableId = 'my_table';
+  // const rows = [
+  //   {name: 'Tom', age: 30},
+  //   {name: 'Jane', age: 32},
+  // ];
+
+  // Insert data into a table
+  await bigquery
+    .dataset(datasetId)
+    .table(tableId)
+    .insert(rows);
+  console.log(`Inserted ${rows.length} rows`);
+}
+
+// run().catch(console.error.bind(console))
